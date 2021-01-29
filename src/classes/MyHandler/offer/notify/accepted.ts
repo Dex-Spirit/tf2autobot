8918542075df99004c8a333cfae5853f6cabf20b
DexSpirit <61100980+Dex-Spirit@users.noreply.github.com>
2021-01-29 17:13:40 +0200

    send custom message first

--- src/classes/MyHandler/offer/notify/accepted.ts
+++ src/classes/MyHandler/offer/notify/accepted.ts
@@ -2,8 +2,8 @@ import { TradeOffer } from 'steam-tradeoffer-manager';
 import Bot from '../../../Bot';
 
 export default function accepted(offer: TradeOffer, bot: Bot): void {
-    bot.sendMessage(offer.partner, '/pre ✅ Success! The offer went through successfully.');
     if (bot.options.customMessage.success) {
         bot.sendMessage(offer.partner, bot.options.customMessage.success);
     }
+    bot.sendMessage(offer.partner, '/pre ✅ Success! The offer went through successfully.');
 }
