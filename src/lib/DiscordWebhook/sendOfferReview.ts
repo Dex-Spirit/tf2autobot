68a9f6bfcab389fc117b40256ec3a34a8b7877c7
DexSpirit <61100980+Dex-Spirit@users.noreply.github.com>
2021-05-31 17:31:34 +0300

    remove message except ID
    for faster copying on mobile

--- src/lib/DiscordWebhook/sendOfferReview.ts
+++ src/lib/DiscordWebhook/sendOfferReview.ts
@@ -35,7 +35,7 @@ export default function sendOfferReview(
     }
     const mentionOwner = noMentionOnInvalidValue
         ? `${offer.id}`
-        : `${opt.offerReview.isMention ? `<@!${opt.ownerID}>, ` : ''}check this! - ${offer.id}`;
+        : `${opt.offerReview.isMention ? `<@!${opt.ownerID}>, check this! - ` : ''}${offer.id}`;
 
     const botInfo = bot.handler.getBotInfo;
     const pureStock = pure.stock(bot);
